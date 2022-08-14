885ce446bd8324a617e860cecfd2b1dd11f7eac2
swc9803 <swc9803@gmail.com>
2022-08-15 03:31:06 +0900

    html element type

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
         "eslint:recommended",
         "@vue/eslint-config-prettier",
     ],
+    "parser": "babel-parser",
     parserOptions: {
         sourceType: "module",
     },
@@ -14,4 +15,8 @@ module.exports = {
         "prettier/prettier": ["error", { tabWidth: 4 }],
         "vue/multi-word-component-names": 0,
     },
+    extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+      ]
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,302 +1,317 @@
 <template>
-    <header class="flex justify-center">
-        <h1>코로나 세계 현황판</h1>
-    </header>
-    <main class="flex">
-        <div class="left-panel flex column">
-            <div class="total-board">
-                <p>Total Confirmed</p>
-                <span class="confirmed-total"></span>
+    <div>
+        <header class="flex justify-center">
+            <h1>코로나 세계 현황판</h1>
+        </header>
+        <main class="flex">
+            <div class="left-panel flex column">
+                <div class="total-board">
+                    <p>Total Confirmed</p>
+                    <span class="confirmed-total"></span>
+                </div>
+                <div class="country-ranks">
+                    <p>Confirmed Cases by Country</p>
+                    <ol class="rank-list"></ol>
+                </div>
+                <p
+                    class="last-updated-time flex justify-center align-center"
+                ></p>
             </div>
-            <div class="country-ranks">
-                <p>Confirmed Cases by Country</p>
-                <ol class="rank-list"></ol>
-            </div>
-            <p class="last-updated-time flex justify-center align-center"></p>
-        </div>
-        <div class="right-panel">
-            <div class="summary-wrapper flex">
-                <div class="deaths-container">
-                    <h3 class="summary-title">Total Deaths</h3>
-                    <p class="total deaths">0</p>
-                    <div class="list-wrapper">
-                        <ol class="deaths-list"></ol>
+            <div class="right-panel">
+                <div class="summary-wrapper flex">
+                    <div class="deaths-container">
+                        <h3 class="summary-title">Total Deaths</h3>
+                        <p class="total deaths">0</p>
+                        <div class="list-wrapper">
+                            <ol class="deaths-list"></ol>
+                        </div>
+                    </div>
+                    <div class="recovered-container">
+                        <h3 class="summary-title">Total Recovered</h3>
+                        <p class="total recovered">0</p>
+                        <div class="list-wrapper">
+                            <ol class="recovered-list"></ol>
+                        </div>
                     </div>
                 </div>
-                <div class="recovered-container">
-                    <h3 class="summary-title">Total Recovered</h3>
-                    <p class="total recovered">0</p>
-                    <div class="list-wrapper">
-                        <ol class="recovered-list"></ol>
-                    </div>
+                <div class="chart-container">
+                    <canvas
+                        id="lineChart"
+                        class="corona-chart"
+                        style="
+                            width: 100%;
+                            height: 356px;
+                            background-color: #5b5656;
+                        "
+                    ></canvas>
                 </div>
             </div>
-            <div class="chart-container">
-                <canvas
-                    id="lineChart"
-                    class="corona-chart"
-                    style="
-                        width: 100%;
-                        height: 356px;
-                        background-color: #5b5656;
-                    "
-                ></canvas>
-            </div>
-        </div>
-    </main>
+        </main>
+    </div>
 </template>
 
 <script setup lang="ts">
-// utils
-function $(selector) {
-    return document.querySelector(selector);
-}
-function getUnixTimestamp(date) {
-    return new Date(date).getTime();
-}
+import { onMounted } from "vue";
+import axios from "axios";
+import { Chart } from "chart.js";
 
-// DOM
-const confirmedTotal = $(".confirmed-total");
-const deathsTotal = $(".deaths");
-const recoveredTotal = $(".recovered");
-const lastUpdatedTime = $(".last-updated-time");
-const rankList = $(".rank-list");
-const deathsList = $(".deaths-list");
-const recoveredList = $(".recovered-list");
-const deathSpinner = createSpinnerElement("deaths-spinner");
-const recoveredSpinner = createSpinnerElement("recovered-spinner");
-
-function createSpinnerElement(id) {
-    const wrapperDiv = document.createElement("div");
-    wrapperDiv.setAttribute("id", id);
-    wrapperDiv.setAttribute(
-        "class",
-        "spinner-wrapper flex justify-center align-center"
-    );
-    const spinnerDiv = document.createElement("div");
-    spinnerDiv.setAttribute("class", "ripple-spinner");
-    spinnerDiv.appendChild(document.createElement("div"));
-    spinnerDiv.appendChild(document.createElement("div"));
-    wrapperDiv.appendChild(spinnerDiv);
-    return wrapperDiv;
-}
-
-// state
-let isDeathLoading = false;
-// eslint-disable-next-line no-unused-vars
-let isRecoveredLoading = false;
-
-// // api
-// /**
-//  * @returns
-//  */
-function fetchCovidSummary() {
-    const url = "https://api.covid19api.com/summary";
-    return axios.get(url);
-}
-
-function fetchCountryInfo(countryCode, status) {
-    // params: confirmed, recovered, deaths
-    const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
-    return axios.get(url);
-}
-
-// methods
-function startApp() {
-    setupData();
-    initEvents();
-}
-
-// events
-function initEvents() {
-    rankList.addEventListener("click", handleListClick);
-}
-
-async function handleListClick(event) {
-    let selectedId;
-    if (
-        event.target instanceof HTMLParagraphElement ||
-        event.target instanceof HTMLSpanElement
-    ) {
-        selectedId = event.target.parentElement.id;
+onMounted(() => {
+    // utils
+    function $(selector: string) {
+        return document.querySelector(selector);
     }
-    if (event.target instanceof HTMLLIElement) {
-        selectedId = event.target.id;
+    function getUnixTimestamp(date: Date) {
+        return new Date(date).getTime();0
     }
-    if (isDeathLoading) {
-        return;
+
+    // DOM                  // $ = document.querySelector
+    const confirmedTotal = $(".confirmed-total") as HTMLSpanElement;
+    const deathsTotal = $(".deaths") as HTMLParagraphElement;
+    const recoveredTotal = $(".recovered") as HTMLParagraphElement;
+    const lastUpdatedTime = $(".last-updated-time") as HTMLParagraphElement;
+    const rankList = $(".rank-list");
+    const deathsList = $(".deaths-list");
+    const recoveredList = $(".recovered-list");
+    const deathSpinner = createSpinnerElement("deaths-spinner");
+    const recoveredSpinner = createSpinnerElement("recovered-spinner");
+
+    function createSpinnerElement(id: string) {
+        const wrapperDiv = document.createElement("div");
+        wrapperDiv.setAttribute("id", id);
+        wrapperDiv.setAttribute(
+            "class",
+            "spinner-wrapper flex justify-center align-center"
+        );
+        const spinnerDiv = document.createElement("div");
+        spinnerDiv.setAttribute("class", "ripple-spinner");
+        spinnerDiv.appendChild(document.createElement("div"));
+        spinnerDiv.appendChild(document.createElement("div"));
+        wrapperDiv.appendChild(spinnerDiv);
+        return wrapperDiv;
     }
-    clearDeathList();
-    clearRecoveredList();
-    startLoadingAnimation();
-    isDeathLoading = true;
-    const { data: deathResponse } = await fetchCountryInfo(
-        selectedId,
-        "deaths"
-    );
-    const { data: recoveredResponse } = await fetchCountryInfo(
-        selectedId,
-        "recovered"
-    );
-    const { data: confirmedResponse } = await fetchCountryInfo(
-        selectedId,
-        "confirmed"
-    );
-    endLoadingAnimation();
-    setDeathsList(deathResponse);
-    setTotalDeathsByCountry(deathResponse);
-    setRecoveredList(recoveredResponse);
-    setTotalRecoveredByCountry(recoveredResponse);
-    setChartData(confirmedResponse);
-    isDeathLoading = false;
-}
 
-function setDeathsList(data) {
-    const sorted = data.sort(
-        (a, b) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
-    );
-    sorted.forEach((value) => {
-        const li = document.createElement("li");
-        li.setAttribute("class", "list-item-b flex align-center");
-        const span = document.createElement("span");
-        span.textContent = value.Cases;
-        span.setAttribute("class", "deaths");
-        const p = document.createElement("p");
-        p.textContent = new Date(value.Date).toLocaleDateString().slice(0, -1);
-        li.appendChild(span);
-        li.appendChild(p);
-        deathsList.appendChild(li);
-    });
-}
+    // state
+    let isDeathLoading = false;
+    // eslint-disable-next-line no-unused-vars
+    let isRecoveredLoading = false;
 
-function clearDeathList() {
-    deathsList.innerHTML = null;
-}
+    function fetchCovidSummary() {
+        const url = "https://api.covid19api.com/summary";
+        return axios.get(url);
+    }
 
-function setTotalDeathsByCountry(data) {
-    deathsTotal.innerText = data[0].Cases;
-}
+    enum CovidStatus {
+        Confirmed = 'confirmed',
+        Recovered = 'recovered',
+        Deaths = 'deaths'
+    }
 
-function setRecoveredList(data) {
-    const sorted = data.sort(
-        (a, b) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
-    );
-    sorted.forEach((value) => {
-        const li = document.createElement("li");
-        li.setAttribute("class", "list-item-b flex align-center");
-        const span = document.createElement("span");
-        span.textContent = value.Cases;
-        span.setAttribute("class", "recovered");
-        const p = document.createElement("p");
-        p.textContent = new Date(value.Date).toLocaleDateString().slice(0, -1);
-        li.appendChild(span);
-        li.appendChild(p);
-        recoveredList.appendChild(li);
-    });
-}
+    function fetchCountryInfo(countryCode: string, status: CovidStatus) {
+        // params: confirmed, recovered, deaths
+        const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
+        return axios.get(url);
+    }
 
-function clearRecoveredList() {
-    recoveredList.innerHTML = null;
-}
+    // methods
+    function startApp() {
+        setupData();
+        initEvents();
+    }
 
-function setTotalRecoveredByCountry(data) {
-    recoveredTotal.innerText = data[0].Cases;
-}
+    // events
+    function initEvents() {
+        rankList.addEventListener("click", handleListClick);
+    }
 
-function startLoadingAnimation() {
-    deathsList.appendChild(deathSpinner);
-    recoveredList.appendChild(recoveredSpinner);
-}
+    async function handleListClick(event: any) {
+        let selectedId;
+        if (
+            event.target instanceof HTMLParagraphElement ||
+            event.target instanceof HTMLSpanElement
+        ) {
+            selectedId = event.target.parentElement.id;
+        }
+        if (event.target instanceof HTMLLIElement) {
+            selectedId = event.target.id;
+        }
+        if (isDeathLoading) {
+            return;
+        }
+        clearDeathList();
+        clearRecoveredList();
+        startLoadingAnimation();
+        isDeathLoading = true;
+        const { data: deathResponse } = await fetchCountryInfo(
+            selectedId, CovidStatus.Deaths
+            );
+        const { data: recoveredResponse } = await fetchCountryInfo(
+            selectedId, CovidStatus.Recovered
+        );
+        const { data: confirmedResponse } = await fetchCountryInfo(
+            selectedId, CovidStatus.Confirmed
+        );
+        endLoadingAnimation();
+        setDeathsList(deathResponse);
+        setTotalDeathsByCountry(deathResponse);
+        setRecoveredList(recoveredResponse);
+        setTotalRecoveredByCountry(recoveredResponse);
+        setChartData(confirmedResponse);
+        isDeathLoading = false;
+    }
 
-function endLoadingAnimation() {
-    deathsList.removeChild(deathSpinner);
-    recoveredList.removeChild(recoveredSpinner);
-}
+    function setDeathsList(data: any) {
+        const sorted = data.sort(
+            (a: any, b: any) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
+        );
+        sorted.forEach((value: any) => {
+            const li = document.createElement("li");
+            li.setAttribute("class", "list-item-b flex align-center");
+            const span = document.createElement("span");
+            span.textContent = value.Cases;
+            span.setAttribute("class", "deaths");
+            const p = document.createElement("p");
+            p.textContent = new Date(value.Date)
+                .toLocaleDateString()
+                .slice(0, -1);
+            li.appendChild(span);
+            li.appendChild(p);
+            deathsList.appendChild(li);
+        });
+    }
 
-async function setupData() {
-    const { data } = await fetchCovidSummary();
-    setTotalConfirmedNumber(data);
-    setTotalDeathsByWorld(data);
-    setTotalRecoveredByWorld(data);
-    setCountryRanksByConfirmedCases(data);
-    setLastUpdatedTimestamp(data);
-}
+    function clearDeathList() {
+        deathsList.innerHTML = null;
+    }
 
-function renderChart(data, labels) {
-    var ctx = $("#lineChart").getContext("2d");
-    Chart.defaults.color = "#f5eaea";
-    Chart.defaults.font.family = "Exo 2";
-    new Chart(ctx, {
-        type: "line",
-        data: {
-            labels,
-            datasets: [
-                {
-                    label: "Confirmed for the last two weeks",
-                    backgroundColor: "#feb72b",
-                    borderColor: "#feb72b",
-                    data,
-                },
-            ],
-        },
-        options: {},
-    });
-}
+    function setTotalDeathsByCountry(data: any) {
+        deathsTotal.innerText = data[0].Cases;
+    }
 
-function setChartData(data) {
-    const chartData = data.slice(-14).map((value) => value.Cases);
-    const chartLabel = data
-        .slice(-14)
-        .map((value) => new Date(value.Date).toLocaleDateString().slice(5, -1));
-    renderChart(chartData, chartLabel);
-}
+    function setRecoveredList(data: any) {
+        const sorted = data.sort(
+            (a: any, b: any) => getUnixTimestamp(b.Date) - getUnixTimestamp(a.Date)
+        );
+        sorted.forEach((value: any) => {
+            const li = document.createElement("li");
+            li.setAttribute("class", "list-item-b flex align-center");
+            const span = document.createElement("span");
+            span.textContent = value.Cases;
+            span.setAttribute("class", "recovered");
+            const p = document.createElement("p");
+            p.textContent = new Date(value.Date)
+                .toLocaleDateString()
+                .slice(0, -1);
+            li.appendChild(span);
+            li.appendChild(p);
+            recoveredList.appendChild(li);
+        });
+    }
 
-function setTotalConfirmedNumber(data) {
-    confirmedTotal.innerText = data.Countries.reduce(
-        (total, current) => (total += current.TotalConfirmed),
-        0
-    );
-}
+    function clearRecoveredList() {
+        recoveredList.innerHTML = null;
+    }
 
-function setTotalDeathsByWorld(data) {
-    deathsTotal.innerText = data.Countries.reduce(
-        (total, current) => (total += current.TotalDeaths),
-        0
-    );
-}
+    function setTotalRecoveredByCountry(data: any) {
+        recoveredTotal.innerText = data[0].Cases;
+    }
 
-function setTotalRecoveredByWorld(data) {
-    recoveredTotal.innerText = data.Countries.reduce(
-        (total, current) => (total += current.TotalRecovered),
-        0
-    );
-}
+    function startLoadingAnimation() {
+        deathsList.appendChild(deathSpinner);
+        recoveredList.appendChild(recoveredSpinner);
+    }
 
-function setCountryRanksByConfirmedCases(data) {
-    const sorted = data.Countries.sort(
-        (a, b) => b.TotalConfirmed - a.TotalConfirmed
-    );
-    sorted.forEach((value) => {
-        const li = document.createElement("li");
-        li.setAttribute("class", "list-item flex align-center");
-        li.setAttribute("id", value.Slug);
-        const span = document.createElement("span");
-        span.textContent = value.TotalConfirmed;
-        span.setAttribute("class", "cases");
-        const p = document.createElement("p");
-        p.setAttribute("class", "country");
-        p.textContent = value.Country;
-        li.appendChild(span);
-        li.appendChild(p);
-        rankList.appendChild(li);
-    });
-}
+    function endLoadingAnimation() {
+        deathsList.removeChild(deathSpinner);
+        recoveredList.removeChild(recoveredSpinner);
+    }
 
-function setLastUpdatedTimestamp(data) {
-    lastUpdatedTime.innerText = new Date(data.Date).toLocaleString();
-}
+    async function setupData() {
+        const { data } = await fetchCovidSummary();
+        setTotalConfirmedNumber(data);
+        setTotalDeathsByWorld(data);
+        setTotalRecoveredByWorld(data);
+        setCountryRanksByConfirmedCases(data);
+        setLastUpdatedTimestamp(data);
+    }
 
-startApp();
+    function renderChart(data: any, labels: any) {
+        const ctx = $("#lineChart").getContext("2d");
+        Chart.defaults.color = "#f5eaea";
+        Chart.defaults.font.family = "Exo 2";
+        new Chart(ctx, {
+            type: "line",
+            data: {
+                labels,
+                datasets: [
+                    {
+                        label: "Confirmed for the last two weeks",
+                        backgroundColor: "#feb72b",
+                        borderColor: "#feb72b",
+                        data,
+                    },
+                ],
+            },
+            options: {},
+        });
+    }
+
+    function setChartData(data: any) {
+        const chartData = data.slice(-14).map((value: any) => value.Cases);
+        const chartLabel = data
+            .slice(-14)
+            .map((value: any) =>
+                new Date(value.Date).toLocaleDateString().slice(5, -1)
+            );
+        renderChart(chartData, chartLabel);
+    }
+
+    function setTotalConfirmedNumber(data: any) {
+        confirmedTotal.innerText = data.Countries.reduce(
+            (total: any, current: any) => (total += current.TotalConfirmed),
+            0
+        );
+    }
+
+    function setTotalDeathsByWorld(data: any) {
+        deathsTotal.innerText = data.Countries.reduce(
+            (total: any, current: any) => (total += current.TotalDeaths),
+            0
+        );
+    }
+
+    function setTotalRecoveredByWorld(data: any) {
+        recoveredTotal.innerText = data.Countries.reduce(
+            (total: any, current: any) => (total += current.TotalRecovered),
+            0
+        );
+    }
+
+    function setCountryRanksByConfirmedCases(data: any) {
+        const sorted = data.Countries.sort(
+            (a: any, b: any) => b.TotalConfirmed - a.TotalConfirmed
+        );
+        sorted.forEach((value: any) => {
+            const li = document.createElement("li");
+            li.setAttribute("class", "list-item flex align-center");
+            li.setAttribute("id", value.Slug);
+            const span = document.createElement("span");
+            span.textContent = value.TotalConfirmed;
+            span.setAttribute("class", "cases");
+            const p = document.createElement("p");
+            p.setAttribute("class", "country");
+            p.textContent = value.Country;
+            li.appendChild(span);
+            li.appendChild(p);
+            rankList.appendChild(li);
+        });
+    }
+
+    function setLastUpdatedTimestamp(data: any) {
+        lastUpdatedTime.innerText = new Date(data.Date).toLocaleString();
+    }
+
+    startApp();
+});
 </script>
 
 <style lang="scss"></style>
